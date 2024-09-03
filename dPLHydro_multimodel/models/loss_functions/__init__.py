d52eca67a98c300fb7297261893991573abb4e73
Leo Lonzarich <lgl5139@psu.edu>
2024-09-03 12:23:03 -0400

    Added NSE loss

--- dPLHydro_multimodel/models/loss_functions/__init__.py
+++ dPLHydro_multimodel/models/loss_functions/__init__.py
@@ -9,11 +9,12 @@ def get_loss_func(args, obs):
     spec = importlib.util.spec_from_file_location("RmseLoss_flow_comb", "/data/lgl5139/hydro_multimodel/dPLHydro_multimodel/models/loss_functions/RmseLoss_flow_comb.py") #args['loss_function'], './models/loss_functions/' + args['loss_function'] + '.py')
     module = spec.loader.load_module()
     loss_function_default = getattr(module, args['loss_function'])
-    if (args['loss_function'] =='RmseLoss_flow_temp') or (args['loss_function'] =='RmseLoss_flow_temp_BFI') or \
-            (args['loss_function'] =='RmseLoss_flow_temp_BFI_PET') or (args['loss_function'] =='RmseLoss_BFI_temp'):
+    
+    if args['loss_function'] in ['RmseLoss_flow_temp', 'RmseLoss_flow_temp_BFI', 'RmseLoss_flow_temp_BFI_PET', 'RmseLoss_BFI_temp']:
         lossFun = loss_function_default(w1=args['loss_function_weights']['w1'],
                                         w2=args['loss_function_weights']['w2'])
-    elif (args['loss_function'] == 'NSEsqrtLoss_flow_temp'):
+    
+    elif args['loss_function'] == 'NSEsqrtLoss_flow_temp':
         std_obs_flow = np.nanstd(obs[:, :, args['target'].index('00060_Mean')], axis=0)
         std_obs_flow[std_obs_flow != std_obs_flow] = 1.0
 
@@ -22,7 +23,8 @@ def get_loss_func(args, obs):
 
         lossFun = loss_function_default(stdarray_flow=std_obs_flow,
                                         stdarray_temp=std_obs_temp)
-    elif (args['loss_function'] == 'NSEsqrtLoss_flow'):
+    
+    elif args['loss_function'] == 'NSEsqrtLoss_flow':
         std_obs_flow = np.nanstd(obs[:, :, args['target'].index('00060_Mean')], axis=0)
         std_obs_flow[std_obs_flow != std_obs_flow] = 1.0
         lossFun = loss_function_default(stdarray_flow=std_obs_flow)
